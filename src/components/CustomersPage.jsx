d81cf46dc00d1db0b0482c2ff1ce5bfb21998694
Bailey Cage <bailey.cage@gmail.com>
2022-10-04 13:18:34 -0400

    setup shell component

---
+++ src/components/CustomersPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CustomersPage() {
+  return (
+    <div>CustomersPage</div>
+  )
+}
+
+export default CustomersPage
---
+++ src/components/RentalsPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function RentalsPage() {
+  return (
+    <div>RentalsPage</div>
+  )
+}
+
+export default RentalsPage
