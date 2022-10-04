cc17448712bbb241105915da83aff68a407eeabd
Bailey Cage <bailey.cage@gmail.com>
2022-10-04 13:18:20 -0400

    add routes for rentals page & customers page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,19 @@
 import Opener from './Opener';
 import Home from './Home';
-import { 
-  BrowserRouter as Router,
-  Switch, 
-  Route,
-  Link
-} from 'react-router-dom';
+import RentalsPage from './components/RentalsPage';
+import CustomersPage from './components/CustomersPage';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import CheckOut from './CheckOut';
 
 function App() {
   return (
-    <>
-      <Router>
-          <>
-          <Link to="/home"></Link>
-
-          <Switch>
-            <Route path="/">
-                <Home />
-            </Route>
-          </Switch>
-          <Switch>
-            <Route path='/checkout'>
-              <CheckOut />
-            </Route>
-          </Switch>
-          </>
-      </Router>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/checkout" element={<CheckOut />} />
+      <Route path="/rentals" element={<RentalsPage />} />
+      <Route path="/customers" element={<CustomersPage />} />
+    </Routes>
   );
 }
 
