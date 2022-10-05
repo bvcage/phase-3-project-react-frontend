ca58c3e291aea792f261d2253438c9f0d7c69261
Ben <berkhart@rocketmail.com>
2022-10-05 00:46:57 -0500

    Created basic POST for check-out button

--- src/MovieDetails.js
+++ src/MovieDetails.js
@@ -6,6 +6,32 @@ function MovieDetails ({ selectedMovie }) {
 
     const { id, image_url, imdb_id, plot, title, year } = selectedMovie
 
+    console.log(selectedMovie)
+
+    const checkOutMovie = () => {
+        fetch('http://localhost:9292/rentals', {
+            method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            "checkout_date": null,
+             "due_date": null,
+             "price": 15,
+             "movie_id": selectedMovie.id,
+             "customer_id": 2
+        }),
+        })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Success:', data);
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+    }
+
+
     return (
         <>
     <div>
@@ -17,7 +43,7 @@ function MovieDetails ({ selectedMovie }) {
         <h2>{title}</h2>
         <h5>{year}</h5>
         <p>{plot}</p>
-        <Button variant='primary'>Check-Out</Button>
+        <Button variant='primary' onClick={checkOutMovie}>Check-Out</Button>
     </div>
     </>
     )
