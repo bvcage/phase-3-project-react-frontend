7bcd04e7e5f872679275153d8e0fafb38a574fd8
Bailey Cage <bailey.cage@gmail.com>
2022-10-05 13:42:34 -0400

    add navigation to home page

--- src/Opener.js
+++ src/Opener.js
@@ -1,29 +1,20 @@
 import React from "react"
+import { useNavigate } from "react-router-dom"
 
 function Opener() {
-    const handleHomePageClick = ()=> {
-            console.log("hello")}
+    const navigate = useNavigate()
+    const handleHomePageClick = () => {
+        console.log("hello")
+        navigate("/home")
+    }
             
     return (
-    <div onClick={handleHomePageClick} className="container-fluid">
-        <h1 className="font">
-            BLOCKBUST3R
-        </h1>
-
-    </div>
+        <div onClick={handleHomePageClick} className="container-fluid">
+            <h1 className="font">
+                BLOCKBUST3R
+            </h1>
+        </div>
     );
 }
 
 export default Opener
-
-  // const navigate = useNavigate
-
-    // const handleHomePageClick = ()=> {
-    //     console.log("hello")
-        
-    //     // navigate('/home')
-
-    //     return (
-           
-    //     )
-    // }
