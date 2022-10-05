df6ea2e63e67154e1d81dff4feaa6db13d0d822c
Bailey Cage <bailey.cage@gmail.com>
2022-10-05 17:07:19 -0400

    implement Row/Col & CRUD buttons

--- src/components/RentalsPage.jsx
+++ src/components/RentalsPage.jsx
@@ -1,6 +1,6 @@
 import "../App.css"
 import React, { useState, useEffect } from "react"
-import { Container, Card, Button } from "react-bootstrap"
+import { Container, Row, Col, Card, Button } from "react-bootstrap"
 import NavBar from "./NavBar"
 
 function RentalsPage () {
@@ -17,9 +17,39 @@ function RentalsPage () {
     rental.rental.checkout_date = new Date(Date.parse(rental.rental.checkout_date))
     rental.rental.due_date = new Date(Date.parse(rental.rental.due_date))
     return (
-      <Card key={rental.rental.id}>
-        {rental.customer.first_name} {rental.customer.last_name} Title: {rental.movie.title} Checkout Date: {(rental.rental.checkout_date).toLocaleDateString()} Due Date: {(rental.rental.due_date).toLocaleDateString()}
-        <Button variant='primary' onClick={() => checkInMovie(rental.rental.id)}>Check-In</Button>
+      <Card key={rental.rental.id} className="m-auto">
+        <Card.Body>
+          <Row>
+            <Col>
+              {rental.customer.first_name} {rental.customer.last_name}
+            </Col>
+            <Col>
+              Title: {rental.movie.title}
+            </Col>
+            <Col>
+              Checkout Date: {(rental.rental.checkout_date).toLocaleDateString()}
+            </Col>
+            <Col>
+              Due Date: {(rental.rental.due_date).toLocaleDateString()}
+            </Col>
+            <Col>
+              <div style={{float: "right"}}>
+                <Button
+                  variant='primary'
+                  onClick={() => checkInMovie(rental.rental.id)}
+                  className="ms-3">
+                    Check-In
+                </Button>
+                <Button
+                  variant='secondary'
+                  onClick={() => extendDueDate(rental.rental.id)}
+                  className="ms-3">
+                    extend
+                </Button>
+              </div>
+            </Col>
+          </Row>
+        </Card.Body>
       </Card>
     )
   })
@@ -29,7 +59,8 @@ function RentalsPage () {
     fetch(`http://localhost:9292/rentals/${rentalId}/delete`, {
       method: 'DELETE',
       headers: {
-          'Content-Type': 'application/json',
+        'Content-Type': 'application/json',
+        "Accept": "application/json"
       }
     })
     .then((response) => response.json())
@@ -41,10 +72,39 @@ function RentalsPage () {
     });
   }
 
+  const extendDueDate = (rentalId) => {
+    const numDaysToAdd = 5
+    const currDueDate = rentalsArr.find(rental => rental.rental.id === rentalId).rental.due_date
+    const newDueDate = new Date();
+    newDueDate.setTime(currDueDate.getTime() + ( numDaysToAdd * (1000 * 60 * 60 * 24)))
+    fetch(`http://localhost:9292/rentals/${rentalId}/edit`, {
+      method: 'PATCH',
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({"due_date": newDueDate})
+    }).then(r=>r.json())
+    .then(data => {
+      let newRental = data.data
+      const newArr = rentalsArr.map(rental => {
+        if (rental.rental.id === newRental.id) {
+          return {...rental,
+            rental: newRental
+          }
+        }
+        return rental
+      })
+      setRentalsArr(newArr)
+    })
+  }
+
   return (
-    <Container>
+    <Container className="p-0 m-auto">
       <NavBar />
-      {rentals}
+      <Container id="rentals-container" className="p-4">
+        {rentals}
+      </Container>
     </Container>
   )
 
