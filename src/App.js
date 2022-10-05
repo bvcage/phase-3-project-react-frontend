188bb226c50a3be36f298367b5212cf76c8db327
Bailey Cage <bailey.cage@gmail.com>
2022-10-05 10:53:44 -0400

    fix route post-merge

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Opener from './Opener';
 import Home from './Home';
 import RentalsPage from './components/RentalsPage';
 import CustomersPage from './components/customers/CustomersPage';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 import CheckOut from './CheckOut';
 
@@ -16,7 +16,7 @@ function App() {
 
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home onClickCheckout={onClickCheckout} />} />
       <Route path="/checkout" element={<CheckOut />} />
       <Route path="/rentals" element={<RentalsPage />} />
       <Route path="/customers" element={<CustomersPage />} />
