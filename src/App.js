7df7cf47f15335446519b7aae95c5ed01e835f1c
Bailey Cage <bailey.cage@gmail.com>
2022-10-06 16:10:28 -0400

    condense spacing, add header & footer

--- src/App.js
+++ src/App.js
@@ -4,113 +4,24 @@ import Opener from './Opener';
 import CheckOut from './CheckOut';
 import RentalsPage from './RentalsPage'
 import CustomersPage from "./components/customers/CustomersPage";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from './Home';
-import { 
-  createBrowserRouter,
-  Routes,
-  Route,
-  RouterProvider,
-  Link,
-  // Routes,
-  useNavigate } from 'react-router-dom';
+import { Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
 
 function App() {
-
-  return (
-<div className='Appz'>
-  <Routes>
-    <Route 
-      path="/"
-      element={
-        <>
-          <Opener />
-        </>
-       }
-    />
-    <Route
-      path="/home"
-      element={
-        <>
-          <Home />
-        </>
-      }
-    />
-    <Route
-      path="/checkout"
-      element={
-        <>
-          <CheckOut />
-        </>
-      }
-    />
-    <Route
-      path="/rentals"
-      element={
-        <>
-          <RentalsPage />
-        </>
-      }
-    />
-    <Route
-      path="/customers"
-      element={
-        <>
-          <CustomersPage />
-        </>
-      }
-    />
-    {/* {
-      path: "/home",
-      element: (
-        <>  
-          <Home />
-          <Link to="checkout">Checkout</Link>
-          <Link to="rentals">Rentals</Link>
-          <Link to="customers">Customers</Link>
-          <div>welcome Home</div>,
-        </>
-      ),
-    },
-    {
-      path: "/checkout",
-      element: (
-        <>  
-          <CheckOut />
-          <Link to="home">Home</Link>
-          <Link to="rentals">Rentals</Link>
-          <Link to="customers">Customers</Link>
-          <div>Checkout</div>,
-        </>
-      ),
-    },
-    {
-      path: "/rentals",
-      element: (
-        <>
-          <Rentals />
-          <Link to="home">Home</Link>
-          <Link to="checkout">Checkout</Link>
-          <Link to="customers">Customers</Link>
-          <div>Rental</div>,
-        </>
-      ),
-    },
-    {
-      path: "/customers",
-      element: (
-        <>
-          <Customers />
-          <Link to="home">Home</Link>
-          <Link to="checkout">Checkout</Link>
-          <Link to="rentals">Rentals</Link>
-          <div>Customers</div>,
-        </>
-      ),
-    }, */}
+  return (<>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Opener />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/checkout" element={<CheckOut />} />
+      <Route path="/rentals" element={<RentalsPage />} />
+      <Route path="/customers" element={<CustomersPage />} />
     </Routes>
-</div>
-  )
-  }
+    <Footer />
+  </>)
+}
 
 export default App;
