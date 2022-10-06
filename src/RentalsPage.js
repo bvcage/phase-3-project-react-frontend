24cb0c1a0e65f26ed4e6c0e801894b1748cd7ea2
Dylan <dylancarver14@gmail.com>
2022-10-06 09:41:53 -0500

    completed DELETE & PATCH functions

--- src/RentalsPage.js
+++ src/RentalsPage.js
@@ -4,55 +4,75 @@ import Button from 'react-bootstrap/Button';
 import React, { useState, useEffect } from "react";
 import { Container } from "react-bootstrap";
 
-function CurrentRentals () {
+function CurrentRentals() {
 
     const [rentalsArr, setRentalsArr] = useState([])
 
     useEffect(() => {
-        fetch("http://localhost:9292/rentals")
-        .then(resp => resp.json())
-        .then((data) => setRentalsArr(data.data))
+        getRentals()
     }, [])
-    
+
+    function getRentals() {
+        fetch("http://localhost:9292/rentals")
+            .then(resp => resp.json())
+            .then((data) => setRentalsArr(data.data))
+    }
+
+    function deleteSelectedRental(id) {
+        fetch(`http://localhost:9292/rentals/${id}/delete`, {
+            method: 'DELETE'
+        }).then((resp) => {
+            resp.json().then((resp) => {
+                console.warn(resp)
+                getRentals()
+            })
+        })
+    }
+
+    function editSelectedRental(rental) {
+
+        const oldCheckoutDate = rental.rental.due_date
+        const newCheckoutDate = new Date()
+        newCheckoutDate.setDate(oldCheckoutDate.getDate() + 2)
+
+        fetch(`http://localhost:9292/rentals/${rental.rental.id}/edit`, {
+            method: 'PATCH',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ due_date: newCheckoutDate })
+        }).then((resp) => {
+            resp.json().then((resp) => {
+                console.warn(resp)
+                getRentals()
+            })
+        })
+    }
+
     const rentals = rentalsArr.map((rental) => {
         rental.rental.checkout_date = new Date(Date.parse(rental.rental.checkout_date))
         rental.rental.due_date = new Date(Date.parse(rental.rental.due_date))
         return (
-            
-            <Card key={rental.rental.id}>
-            {rental.customer.first_name} {rental.customer.last_name} Title: {rental.movie.title} Checkout Date: {(rental.rental.checkout_date).toLocaleDateString()} Due Date: {(rental.rental.due_date).toLocaleDateString()}
-            </Card>
-            <Button variant='primary' onClick={checkInMovie}>Check-In</Button>
+            <div>
+                <Card key={rental.rental.id}>
+                    {rental.customer.first_name} {rental.customer.last_name} Title: {rental.movie.title} Checkout Date: {(rental.rental.checkout_date).toLocaleDateString()} Due Date: {(rental.rental.due_date).toLocaleDateString()}
+                </Card>
+                <div>
+                    <Button onClick={() => deleteSelectedRental(rental.rental.id)} className="Delete">
+                        Check-In
+                    </Button>
+                    <Button onClick={() => editSelectedRental(rental)}>
+                        Extend Due Date 2 Days
+                    </Button>
+                </div>
+            </div>
         )
     })
-
-    const checkInMovie = () => {
-        fetch('http://localhost:9292/rentals', {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                "checkout_date": null,
-                "due_date": null,
-                "price": 15,
-                "movie_id": selectedMovie.id,
-                "customer_id": 3
-            }),
-        })
-        .then((response) => response.json())
-        .then((data) => {
-            console.log('Success:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-    }
 
     return (
         <Container>
             {rentals}
-            
         </Container>
     )
 
