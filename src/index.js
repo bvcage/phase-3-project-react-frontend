d5b3173f59572a57fd861228aaba9c098b0a864c
Bailey Cage <bailey.cage@gmail.com>
2022-10-06 16:10:02 -0400

    remove unused imports

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { 
-  BrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-} from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import './index.css'
 import App from './App';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
