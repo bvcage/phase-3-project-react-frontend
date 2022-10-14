c690d2c5c583271a07ae6aede4b85cd3d2244727
Bailey Cage <bailey.cage@gmail.com>
2022-10-14 14:26:03 -0400

    add sort

--- src/components/movies/CheckOut.jsx
+++ src/components/movies/CheckOut.jsx
@@ -12,32 +12,87 @@ function CheckOut () {
     const [selectedMovie, setSelectedMovie] = useState()
     const [searchTerm, setSearchTerm] = useState('')
     const [filterMovieList, setFilterMovieList] = useState([])
+    const [sortBy, setSortBy] = useState('')
 
-   useEffect(() => {
-    fetch("http://localhost:9292/movies")
-    .then((res) => res.json())
-    .then((data) => setMoviesArr(data.data))
-   }, [])
+    useEffect(() => {
+        fetch("http://localhost:9292/movies")
+        .then((res) => res.json())
+        .then((data) => setMoviesArr(data.data))
+    }, [])
 
-   useEffect(() => {
-        setFilterMovieList(moviesArr.filter((movie) => movie.title.toLowerCase().includes(searchTerm.toLowerCase()) || movie.genre.includes(searchTerm)))
-   }, [searchTerm])
+    // useEffect(() => {
+    //     setFilterMovieList(moviesArr.filter((movie) => movie.title.toLowerCase().includes(searchTerm.toLowerCase()) || movie.genre.includes(searchTerm)))
+    // }, [searchTerm])
 
 
-   const handleClickMovie = (movie) => {
-       setSelectedMovie(movie);
-   }
+    const handleClickMovie = (movie) => {
+        setSelectedMovie(movie);
+    }
 
-    const movies = (searchTerm.length ? filterMovieList : moviesArr)?.map((movie) => {
+    // const movies = (searchTerm.length ? filterMovieList : moviesArr)?.map((movie) => {
+    //     return (
+    //         <MovieCard movie={movie} key={movie.id} onClickMovie={handleClickMovie}/>
+    //     );
+    // });
+
+
+    // sort movies
+    let movies = moviesArr
+    switch (sortBy) {
+        case 'a to z':
+            movies = moviesArr.sort((a,b) => {
+                const titleA = a.title.toUpperCase()
+                const titleB = b.title.toUpperCase()
+                if (titleA < titleB) { return -1 }
+                if (titleA > titleB) { return 1 }
+                return 0
+            })
+            break
+        case 'z to a':
+            movies = moviesArr.sort((a,b) => {
+                const titleA = a.title.toUpperCase()
+                const titleB = b.title.toUpperCase()
+                if (titleA > titleB) { return -1 }
+                if (titleA < titleB) { return 1 }
+                return 0
+            })
+            break
+        case 'year ↑':
+            movies = moviesArr.sort((a,b) => {
+                if (a.year > b.year) { return 1 }
+                if (a.year < b.year) { return -1 }
+                return 0
+            })
+            break
+        case 'year ↓':
+            movies = moviesArr.sort((a,b) => {
+                if (a.year < b.year) { return 1 }
+                if (a.year > b.year) { return -1 }
+                return 0
+            })
+            break
+        default:
+            movies = moviesArr
+            break
+    }
+
+    // filter movies
+    movies = movies.filter((movie) => movie.title.toLowerCase().includes(searchTerm.toLowerCase()) || movie.genre.includes(searchTerm))
+
+    // create movie cards
+    movies = movies.map(movie => {
         return (
             <MovieCard movie={movie} key={movie.id} onClickMovie={handleClickMovie}/>
         );
-    });
+    })
 
 
     return (
         <Container className='page-container'>
-            <MoviesSearchBar setSearchTerm={setSearchTerm}/>
+            <MoviesSearchBar
+                setSearchTerm={setSearchTerm}
+                setSortBy={setSortBy}
+            />
             {selectedMovie ?
                 <MovieDetails selectedMovie={selectedMovie} setSelectedMovie={setSelectedMovie} />
             :
--- src/components/movies/MoviesSearchBar.jsx
+++ src/components/movies/MoviesSearchBar.jsx
@@ -1,62 +1,88 @@
-import "../../App.css"
-import Form from 'react-bootstrap/Form'
-import Dropdown from 'react-bootstrap/Dropdown';
-import DropdownButton from 'react-bootstrap/DropdownButton';
-import InputGroup from 'react-bootstrap/InputGroup';
-import { useEffect, useState } from "react";
-import { Container } from "react-bootstrap";
+import { useEffect, useState } from "react"
+import { Col, Container, Dropdown, DropdownButton, Form, InputGroup, Row } from "react-bootstrap"
 
-function MoviesSearchBar ({ setSearchTerm }) {
+function MoviesSearchBar ( props ) {
+  const { setSearchTerm, setSortBy } = props
     const [genresArr, setGenresArr] = useState([])
     const [genreLabel, setGenreLabel] = useState('')
+    const [sortLabel, setSortLabel] = useState('')
 
     useEffect(() => {
-        fetch('http://localhost:9292/movies/genres')
-        .then ((res) => res.json())
-        .then ((data) => setGenresArr(data.data))
+      fetch('http://localhost:9292/movies/genres')
+      .then ((res) => res.json())
+      .then ((data) => setGenresArr(data.data))
     }, [])
 
     const onChangeHandler = (e) => {
-        setSearchTerm(e.target.value)
+      setSearchTerm(e.target.value)
     }
 
     const onClickGenre = (e) => {
-        setSearchTerm(e.target.innerText)
-        setGenreLabel(e.target.innerText)
+      setSearchTerm(e.target.innerText)
+      setGenreLabel(e.target.innerText)
+    }
+
+    const onClickSort = (e) => {
+      setSortLabel(e.target.innerText)
+      setSortBy(e.target.innerText)
     }
 
     const resetSearchTerm = () => {
-        setSearchTerm('')
-        setGenreLabel('')
+      setSearchTerm('')
+      setGenreLabel('')
     }
 
     const genres = genresArr.map((genre) =>{
       return (
-        <Dropdown.Item key={genre} href="#" onClick={onClickGenre}>{genre}</Dropdown.Item>
+        <Dropdown.Item key={genre} onClick={onClickGenre}>{genre}</Dropdown.Item>
+      )
+    })
+
+    const sortOpts = ['a to z', 'z to a', 'year ↑', 'year ↓']
+    const sorts = sortOpts.map((sort, i) => {
+      return (
+        <Dropdown.Item key={i} onClick={onClickSort}>{sort}</Dropdown.Item>
       )
     })
 
 
     return (
       <Container className='search-bar'>
-        <InputGroup style={{display: 'flex', justifyContent: 'center'}}>
-          <DropdownButton
-            variant="primary"
-            title={genreLabel ? genreLabel : "Genre"}
-            id="input-group-dropdown-1"
-          >
-            <Container style={{overflowY: 'scroll', maxHeight: '200px', backgroundColor: 'white'}}>
-            <Dropdown.Item href="#" onClick={resetSearchTerm}>All Movies</Dropdown.Item>
-            {genres}
-            </Container>
-          </DropdownButton>
-          <Form.Control aria-label="Search for a movie..."
-            placeholder="Search for a movie..."
-            style={{maxWidth: '1200px' }}
-            onChange={onChangeHandler}
-            onClick={resetSearchTerm}
-            />
-        </InputGroup>
+        <Form>
+          <Form.Group as={Row}>
+            <Col>
+              <InputGroup>
+                <DropdownButton
+                  variant="primary"
+                  title={genreLabel ? genreLabel : "genre"}
+                  id="input-group-dropdown-1"
+                >
+                  <Container style={{overflowY: 'scroll', maxHeight: '200px', backgroundColor: 'white'}}>
+                    <Dropdown.Item href="#" onClick={resetSearchTerm}>All Movies</Dropdown.Item>
+                    {genres}
+                  </Container>
+                </DropdownButton>
+                <Form.Control aria-label="search for a movie..."
+                  placeholder="search for a movie..."
+                  style={{maxWidth: '1200px' }}
+                  onChange={onChangeHandler}
+                  onClick={resetSearchTerm}
+                  />
+              </InputGroup>
+            </Col>
+            <Col xs='auto' className='ps-0'>
+                <DropdownButton
+                  variant="primary"
+                  title={sortLabel ? sortLabel : "sort"}
+                  id="input-group-dropdown-1"
+                >
+                  <Container style={{overflowY: 'scroll', maxHeight: '200px', backgroundColor: 'white'}}>
+                    {sorts}
+                  </Container>
+                </DropdownButton>
+            </Col>
+          </Form.Group>
+        </Form>
       </Container>
     )
 }
